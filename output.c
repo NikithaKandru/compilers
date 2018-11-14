64b94193e590e32b4c63aa5c8ca91f5c271dfbbe
Nikitha Kandru <nkandru@cn-mem.cm.cluster>
2018-11-14 16:11:35 -0500

    output

---
+++ output.c
@@ -0,0 +1,17 @@
+Iterating over function main!
+Outermost loop is Loop ID #0 
+  Loop #1 is nested inside Loop #0
+    OuterLoop Loop ID #0 with InstCount 9 and BasicBlockCount 2
+Iterating over function Sum!
+Outermost loop is Loop ID #0 
+  Loop #1 is nested inside Loop #0
+          Loop ID #1 with InstCount 1 and BasicBlockCount 1
+  OuterLoop Loop ID #0 with InstCount 17 and BasicBlockCount 5
+Iterating over function Prod!
+Outermost loop is Loop ID #0 
+  Loop #1 is nested inside Loop #0
+          Loop ID #1 with InstCount 8 and BasicBlockCount 1
+  OuterLoop Loop ID #0 with InstCount 23 and BasicBlockCount 5
+Outermost loop is Loop ID #1 
+  Loop #2 is nested inside Loop #1
+    OuterLoop Loop ID #1 with InstCount 10 and BasicBlockCount 2
